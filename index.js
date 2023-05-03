44d4dbcb176fac70289cebfa1064da2305e3175d
Christopher Lozzi <clozzi12@gmail.com>
2023-05-03 18:40:42 -0400

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.toUpperCase() === name.toUpperCase();
+    });
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.startsWith(name);
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function(driver) {
+        return driver.name === name;
+    });
+}
